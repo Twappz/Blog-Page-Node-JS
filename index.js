9829a659e3d0ffc5ea7fbf2927b5dd1c444d2f29
Rontab <rontab95@yahoo.com>
2022-12-03 21:58:15 -0800

    index.js

--- index.js
+++ index.js
@@ -78,9 +78,9 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+// app.listen(3000, function() {
+//   console.log("Server started on port 3000");
+// });
 
 let port = process.env.PORT;
 if (port === null || port == "") {
